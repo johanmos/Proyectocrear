edcdd32c89ab4a3a13bbc3ec49284aa3cf38f7d8
Camilo <ingcamiloibarrayepes@hotmail.com>
2017-02-02 16:11:22 -0500

    Access-denied
    se modifico diseño access denied con includes

---
+++ access-denied.php
@@ -0,0 +1,459 @@
+<!DOCTYPE html>
+
+<html class="supports-js supports-no-touch supports-csstransforms supports-csstransforms3d supports-fontface">
+<?php
+					require_once('conexion.php');
+				
+							
+						$link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD);
+						if(!$link) {
+							die('Failed to connect to server: ' . mysql_error());		
+							}
+						
+						//Select database
+						$db = mysqli_select_db($link,DB_DATABASE);
+						if(!$db) {
+							die("Unable to select database");
+						}
+				
+					
+?>
+<!DOCTYPE html>
+<html class="supports-js supports-no-touch supports-csstransforms supports-csstransforms3d supports-fontface">
+
+<!-- Mirrored from demo.tadathemes.com/HTML_Homemarket/demo/todays-deals.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 16 Nov 2016 17:20:22 GMT -->
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta charset="utf-8">
+	<title>Acceso denegado</title>
+	<!-- Font ================================================== -->
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700"> 
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Lato:300,400,500,600,700">
+	<!-- Helpers ================================================== -->
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="Home Market - Responsive HTML5 theme">
+	<meta property="og:image" content="../../assets/images/logo.html">
+	<meta property="og:image:secure_url" content="../../assets/images/logo.html">
+	<meta property="og:url" content="#">
+	<meta property="og:site_name" content="Home Market Red">
+	<meta name="twitter:site" content="@">
+	<meta name="twitter:card" content="summary">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<!-- CSS ================================================== -->
+	<link href="assets/css/jquery.fancybox.css" rel="stylesheet" type="text/css" media="all">
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+	<link href="assets/css/animate.min.css" rel="stylesheet" type="text/css" media="all">
+	<link href="assets/css/swatch.css" rel="stylesheet" type="text/css" media="all">
+	<link href="assets/css/owl.carousel.min.css" rel="stylesheet" type="text/css" media="all">
+	<link href="assets/css/flexslider.css" rel="stylesheet" type="text/css" media="all">
+	<link href="assets/css/timber.scss.css" rel="stylesheet" type="text/css" media="all">
+	<link href="assets/css/home_market.global.scss.css" rel="stylesheet" type="text/css" media="all">
+	<link href="assets/css/home_market.style.scss.css" rel="stylesheet" type="text/css" media="all">
+	<link href="assets/css/tada.css" rel="stylesheet" type="text/css" media="all">
+	<link href="assets/css/spr.css" rel="stylesheet" type="text/css" media="all">
+	<!-- JS ================================================== -->
+	<script src="assets/js/jquery.min.js" type="text/javascript"></script>	
+	<script src="assets/js/jquery.fancybox.min.js" type="text/javascript"></script>
+	<script src="assets/js/owl.carousel.min.js" type="text/javascript"></script>
+	<script src="assets/js/jquery.tweet.min.js" type="text/javascript"></script>
+	<script src="assets/js/jquery.optionSelect.js" type="text/javascript"></script>
+	<script src="assets/js/jquery.flexslider-min.js" type="text/javascript"></script>
+</head>
+
+<body id="products" class="template-collection">
+	<div id="NavDrawer" class="drawer drawer--left">
+		<div class="drawer__header">
+			<div class="drawer__title h3">
+				Browse
+			</div>
+			<div class="drawer__close js-drawer-close">
+				<button type="button" class="icon-fallback-text">
+				<span class="icon icon-x" aria-hidden="true"></span>
+				<span class="fallback-text">Close menu</span>
+				</button>
+			</div>
+		</div>
+		<!-- begin mobile-nav -->
+		<?php include('includes/menu_movil.php');?>
+	</div>
+	
+	<div id="CartDrawer" class="drawer drawer--right fancybox-margin">
+		<div class="drawer__header">
+			<div class="drawer__title h3">
+				Shopping Cart
+			</div>
+			<div class="drawer__close js-drawer-close">
+				<button type="button" class="icon-fallback-text">
+				<span class="icon icon-x" aria-hidden="true"></span>
+				<span class="fallback-text">"Close Cart"</span>
+				</button>
+			</div>
+		</div>
+		<div id="CartContainer">
+			<form action="http://demo.tadathemes.com/HTML_Homemarket/demo/cart.html" method="post" novalidate="" class="cart ajaxcart">
+				<div class="ajaxcart__inner">
+					<div class="ajaxcart__product">
+						<div class="ajaxcart__row" data-line="1">
+							<div class="grid">
+								<div class="grid__item one-quarter">
+									<a href="product.html" class="ajaxcart__product-image"><img src="assets/images/demo1_cart1.jpg" alt=""></a>
+								</div>
+								<div class="grid__item three-quarters">
+									<p>
+										<a href="product.html" class="ajaxcart__product-name">Demo Product Sample</a>
+										<span class="ajaxcart__product-meta">S / Red</span>
+									</p>
+									<div class="grid--full display-table">
+										<div class="grid__item">
+											<div class="ajaxcart__qty">
+												<button type="button" class="ajaxcart__qty-adjust ajaxcart__qty--minus icon-fallback-text" data-id="8772444163" data-qty="0" data-line="1">
+												<span class="icon icon-minus" aria-hidden="true"></span>
+												<span class="fallback-text">?</span>
+												</button>
+												<input type="text" name="updates[]" class="ajaxcart__qty-num" value="1" min="0" data-id="8772444163" data-line="1" aria-label="quantity" pattern="[0-9]*">
+												<button type="button" class="ajaxcart__qty-adjust ajaxcart__qty--plus icon-fallback-text" data-id="8772444163" data-line="1" data-qty="2">
+												<span class="icon icon-plus" aria-hidden="true"></span>
+												<span class="fallback-text">+</span>
+												</button>
+											</div>
+										</div>
+										<div class="grid__item">
+											<span class="money" data-currency-usd="$34.00 USD" data-currency="USD">$34.00 USD</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="ajaxcart__product">
+						<div class="ajaxcart__row" data-line="2">
+							<div class="grid">
+								<div class="grid__item one-quarter">
+									<a href="product.html" class="ajaxcart__product-image"><img src="assets/images/demo1_cart2.jpg" alt=""></a>
+								</div>
+								<div class="grid__item three-quarters">
+									<p>
+										<a href="product.html" class="ajaxcart__product-name">Demo Product Sample</a>
+										<span class="ajaxcart__product-meta">Medium / Pink</span>
+									</p>
+									<div class="grid--full display-table">
+										<div class="grid__item">
+											<div class="ajaxcart__qty">
+												<button type="button" class="ajaxcart__qty-adjust ajaxcart__qty--minus icon-fallback-text" data-id="10722484483" data-qty="0" data-line="2">
+												<span class="icon icon-minus" aria-hidden="true"></span>
+												<span class="fallback-text">?</span>
+												</button>
+												<input type="text" name="updates[]" class="ajaxcart__qty-num" value="1" min="0" data-id="10722484483" data-line="2" aria-label="quantity" pattern="[0-9]*">
+												<button type="button" class="ajaxcart__qty-adjust ajaxcart__qty--plus icon-fallback-text" data-id="10722484483" data-line="2" data-qty="2">
+												<span class="icon icon-plus" aria-hidden="true"></span>
+												<span class="fallback-text">+</span>
+												</button>
+											</div>
+										</div>
+										<div class="grid__item">
+											<span class="money" data-currency-usd="$100.00 USD" data-currency="USD">$100.00 USD</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="ajaxcart__product">
+						<div class="ajaxcart__row" data-line="3">
+							<div class="grid">
+								<div class="grid__item one-quarter">
+									<a href="product.html" class="ajaxcart__product-image"><img src="assets/images/demo1_cart3.jpg" alt=""></a>
+								</div>
+								<div class="grid__item three-quarters">
+									<p>
+										<a href="product.html" class="ajaxcart__product-name">Demo Product Sample</a>
+										<span class="ajaxcart__product-meta">XS / Black</span>
+									</p>
+									<div class="grid--full display-table">
+										<div class="grid__item">
+											<div class="ajaxcart__qty">
+												<button type="button" class="ajaxcart__qty-adjust ajaxcart__qty--minus icon-fallback-text" data-id="8772462979" data-qty="0" data-line="3">
+												<span class="icon icon-minus" aria-hidden="true"></span>
+												<span class="fallback-text">?</span>
+												</button>
+												<input type="text" name="updates[]" class="ajaxcart__qty-num" value="1" min="0" data-id="8772462979" data-line="3" aria-label="quantity" pattern="[0-9]*">
+												<button type="button" class="ajaxcart__qty-adjust ajaxcart__qty--plus icon-fallback-text" data-id="8772462979" data-line="3" data-qty="2">
+												<span class="icon icon-plus" aria-hidden="true"></span>
+												<span class="fallback-text">+</span>
+												</button>
+											</div>
+										</div>
+										<div class="grid__item">
+											<span class="money" data-currency-usd="$89.00 USD" data-currency="USD">$89.00 USD</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div>
+						<label for="CartSpecialInstructions">Special instructions for seller</label>
+						<textarea name="note" class="input-full" id="CartSpecialInstructions"></textarea>
+					</div>
+				</div>
+				<div class="ajaxcart__footer">
+					<div class="grid--full">
+						<div class="grid__item title-total">
+							<p>
+								Subtotal
+							</p>
+						</div>
+						<div class="grid__item price-total">
+							<p>
+								<span class="money" data-currency-usd="$223.00 USD" data-currency="USD">$223.00 USD</span>
+							</p>
+						</div>
+					</div>
+					<p class="text-center">
+						Shipping &amp; taxes calculated at checkout
+					</p>
+					<button type="button" class="btn btn--full cart__shoppingcart" name="shoppingCart" onclick="location.href='cart.html'">
+					Shopping Cart ? </button>
+					<button type="submit" class="btn btn2 btn--full cart__checkout" name="checkout">
+					Check Out ? </button>
+				</div>
+			</form>
+		</div>
+	</div>
+	
+	<div id="PageContainer" class="is-moved-by-drawer">
+		<!-- Ads Banner -->
+		<div id="adv-banner">
+			<div id="ads-banner" class="grid--full grid--table">
+				<div class="ads-banner-slider owl-carousel">
+					<div class="ads-item">
+						<img src="assets/images/ads1.png" alt="ads banner 1">
+					</div>						
+					<div class="ads-item">
+						<img src="assets/images/ads2.png" alt="ads banner 2">
+					</div>
+					<div class="ads-item">
+						<img src="assets/images/ads3.png" alt="ads banner 3">
+					</div>
+				</div>
+			</div>
+		</div>
+	
+		<!-- Top Other -->
+		<?php include('includes/top_info.php');?>
+
+		<!-- Main Header -->
+		<header class="site-header">
+			<div class="wrapper">
+				<div id="main-header" class="grid--full grid--table">
+					<?php include('includes/logo.php');?>
+					<div class="grid__item small--one-whole medium--one-whole four-eighths mobile-bottom">
+						<div class="large--hide medium-down--show navigation-icon">
+							<div class="grid">
+								<div class="grid__item one-half">
+									<div class="site-nav--mobile">
+										<button type="button" class="icon-fallback-text site-nav__link js-drawer-open-left" aria-controls="NavDrawer" aria-expanded="false">
+										<span class="icon icon-hamburger" aria-hidden="true"></span>
+										<span class="fallback-text">Menu</span>
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+						<?php include('includes/busqueda.php'); ?>
+						<div class="large--hide medium-down--show navigation-cart">
+							<div class="grid__item text-right">
+								<div class="site-nav--mobile">
+									<a href="cart.html" class="js-drawer-open-right site-nav__link" aria-controls="CartDrawer" aria-expanded="false">
+									<span class="icon-fallback-text">
+									<span class="icon icon-cart" aria-hidden="true"></span>
+									<span class="fallback-text">Shopping Cart</span>
+									</span>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<?php include('includes/options_header.php');?>
+				</div>       
+			</div>
+		</header>
+
+		<!-- Navigation Bar -->
+		<nav class="nav-bar">
+			<div class="wrapper">
+				<div class="medium-down--hide">
+					<!-- begin site-nav -->
+					<?php include('includes/menu_principal.php');?>
+					<script>
+						  $(window).ready(function($) {
+							//$('.megamenu__dropdown').css("width",$('#main-header').innerWidth());
+						  });
+						  $( window ).resize(function() {
+							//$('.megamenu__dropdown').css("width",$('#main-header').innerWidth());
+						  });
+					</script>
+				</div>
+			</div>
+		</nav>    <!-- Main Content -->
+    
+		<main class="main-content">
+			<br><br>
+			<div class="wrapper">
+				<div class="grid">
+					<div class="grid__item">
+						
+
+						<div class="page-content">      
+							
+						
+									
+								<br>
+									<center><h2>No tiene autorización para entrar aquí.</h2></center>
+								<br>
+								<br>
+								 
+												
+												
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
+		<footer class="site-footer">       
+			<?php include('includes/info_footer.php'); ?>
+			
+			<script type="text/javascript">
+			  $(function () {
+				$(".fi-title").click(function(){
+				  $(this).toggleClass('opentab');
+				});
+			  });
+			</script>         
+		</footer>
+	</div>
+
+	<div id="scroll-to-top" title="Scroll to Top" class="off">
+		<i class="fa fa-caret-up"></i>
+	</div>
+    
+    <script>
+		var tada_index,tada_autosearchcomplete,tada_swiftype,tada_ads,tada_adsspeed,tada_slideshowtime,tada_block1gallery=false,tada_block1product=false, tada_newsletter=false;
+		  tada_index=1;
+		  tada_ads=1;
+		  tada_adsspeed=5000;
+		  tada_slideshowtime = 30000;
+		  tada_block1gallery = true;
+		  tada_block1product = true;
+		  tada_newsletter = true;
+    </script>
+  
+	<script src="assets/js/modernizr.min.js" type="text/javascript"></script>
+	<script src="assets/js/timber.js" type="text/javascript"></script>
+  
+	<div id="quick-shop-modal" class="modal quick-shop" style="display:none;">
+		<div class="modal-dialog fadeIn">
+			<div class="modal-content">
+				<div class="modal-body">
+					<div class="quick-shop-modal-bg">
+					</div>
+					<div class="grid__item one-half qs-product-image">
+						<div id="quick-shop-image" class="product-image-wrapper">
+							<div id="featuted-image" class="main-image featured">
+								<img class="img-zoom img-responsive image-fly" src="assets/images/demo1_qs_480x480.jpg" data-zoom-image="assets/images/demo1_qs_480x480.jpg" alt="">
+							</div>
+							<div id="gallery_main_qs" class="product-image-thumb scroll scroll-mini">
+								<div class="qs-vertical-slider product-single__thumbnails">
+										<a class="image-thumb active thumb__element"><img src="assets/images/demo1_qs1_100x100.jpg" alt="" /></a>
+										<a class="image-thumb thumb__element"><img src="assets/images/demo1_qs2_100x100.jpg" alt="" /></a>
+										<a class="image-thumb thumb__element"><img src="assets/images/demo1_qs3_100x100.jpg" alt="" /></a>
+										<a class="image-thumb thumb__element"><img src="assets/images/demo1_qs4_100x100.jpg" alt="" /></a>
+										<a class="image-thumb thumb__element"><img src="assets/images/demo1_qs5_100x100.jpg" alt="" /></a>										
+								</div>
+							</div>
+							<div class="vertical-slider product-single__thumbnails" style="opacity: 0;">
+							</div>
+						</div>
+					</div>
+					<div class="grid__item one-half qs-product-information">
+						<div id="quick-shop-container">
+							<h3 id="quick-shop-title"><a href="product.html">Corporis suscipit laboriosam</a></h3>
+							<div class="rating-star">
+								<span class="shopify-product-reviews-badge" data-id="3008529923"></span>
+							</div>
+							<div class="description">
+								<div id="quick-shop-description" class="text-left">
+									<p>
+										Quisque vel enim quis purus ultrices consequat, sed tincidunt massa blandit ipsum interdum tristique cras dictum, lacus eu molestie elementum nulla est auctor. Etiam dan lorem quis ligula elementum porttitor quisem. Duis eget purus urna fusce sed scelerisque ante. Lorem ipsum dolor sit amet conse...
+									</p>
+								</div>
+							</div>
+							<form action="#" method="post" class="variants" id="AddToCartForm" enctype="multipart/form-data">
+								<div id="quick-shop-price-container" class="detail-price">
+									<span class="price"><span class="money">$89.00</span></span>
+								</div>
+								<div class="quantity-wrapper clearfix">
+									<label class="wrapper-title">Quantity</label>
+									<div class="wrapper">
+										<span class="qty-down" title="Decrease" data-src="#qs-quantity">
+										<i class="fa fa-minus"></i>
+										</span>
+										<input type="text" id="qs-quantity" size="5" class="item-quantity" name="quantity" value="1">
+										<span class="qty-up" title="Increase" data-src="#qs-quantity">
+										<i class="fa fa-plus"></i>
+										</span>
+									</div>
+								</div>
+								<div id="quick-shop-variants-container" class="variants-wrapper" style="display: block;">
+									<div class="selector-wrapper">
+										<label for="#quick-shop-variants-3008529731-option-0">Size</label>
+										<select class="single-option-selector" data-option="option1" id="#quick-shop-variants-3008529731-option-0">
+											<option value="XS">XS</option>
+											<option value="S">S</option>
+											<option value="M">M</option>
+											<option value="L">L</option>
+											<option value="XL">XL</option>
+										</select>
+									</div>
+									<div class="selector-wrapper">
+										<label for="#quick-shop-variants-3008529731-option-1">Color</label>
+										<select class="single-option-selector" data-option="option2" id="#quick-shop-variants-3008529731-option-1">
+											<option value="Black">Black</option>
+											<option value="Red">Red</option>
+											<option value="Green">Green</option>
+											<option value="Blue">Blue</option>
+											<option value="White">White</option>
+										</select>
+									</div>
+								</div>
+								<div class="others-bottom">
+									<input id="AddToCart" class="btn btn-1 small add-to-cart" type="submit" name="add" value="Buy Now">
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script type="text/javascript">	  
+	  jQuery(document).ready(function($) {  
+		if($('.quantity-wrapper').length){
+		  $('.quantity-wrapper').on('click', '.qty-up', function() {
+			var $this = $(this);
+			var qty = $this.data('src');
+			$(qty).val(parseInt($(qty).val()) + 1);
+		  });
+		  $('.quantity-wrapper').on('click', '.qty-down', function() {
+			var $this = $(this);
+			var qty = $this.data('src');
+			if(parseInt($(qty).val()) > 1)
+			  $(qty).val(parseInt($(qty).val()) - 1);
+		  });
+		}	 			 
+	  });
+	</script>
+	
+</body>
